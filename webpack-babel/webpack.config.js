ac857cc3a85bc7d72f6c35b744556d43eca9c030
wangjunzmd@163.com <wangjunzmd@163.com>
2019-12-30 10:34:29 +0800

    打包less
    Signed-off-by: wangjunzmd@163.com <wangjunzmd@163.com>

--- webpack-babel/webpack.config.js
+++ webpack-babel/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 
-
 module.exports = {
   mode: "development",
   entry: {
@@ -9,43 +8,56 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, "./dist"),
-    filename: "js/[name].bundle.js"
+    filename: "js/[name].[hash].js"
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        loader: "babel-loader" 
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('autoprefixer')
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "less-loader"
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new htmlWebpackPlugin({
       filename: "app.bundle.html",
       template: "./app.html"
-    }),
+    })
   ],
-  module: {
-      rules: [
-        { 
-          test: /\.js$/, 
-          include: /src/, 
-          loader: "babel-loader" 
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader"
-            },
-            {
-              loader: "postcss-loader",
-              options: {
-                ident: 'postcss',
-                plugins: [
-                  require('autoprefixer')
-                ]
-              }
-            }
-          ]
-        }
-      ]
-  }
 };
 
 
